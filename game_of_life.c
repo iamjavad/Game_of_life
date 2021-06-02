4d1dba434950e3579403e92fece7d32d957fdcd8
javad pournosrat <pournosrat.javad@gmail.com>
2021-06-02 11:16:18 +0430

    compiled!

--- game_of_life.c
+++ game_of_life.c
@@ -4,19 +4,33 @@
 void draw(void *u, int w, int h)
 {
 		char (*world)[w] = u;
+		printf("\033[H");
 		for (int y = 0; y < h; y++){
 				for (int x = 0; x < w; x++)
 							printf(world[y][x]? "+" : ".");
 				printf("\n");
 		}
-
 }
 
 void evolution(void *u, int w, int h)
 {
-
+		char (*world)[w] = u;
+		char new[h][w];
+		for (int x = 0; x < w; x++)
+				for (int y = 0; y < h; y++) {
+						int lives = 0;
+						for (int xd = x -1; xd <= x + 1; xd++)
+								for (int yd = y - 1; yd <= y + 1; yd++)
+										if (world[(yd + h) % h][(xd + w) % w])
+												lives++;
+						if (world[y][x]) lives--;
+						
+						new[y][x] = (lives==3 || (lives==2 && world[y][x]));
+						}	
+		for (int y = 0; y < h; y++)
+				for (int x = 0; x < w; x++)
+						world[y][x];
 }
-
 
 int main()
 {
@@ -28,5 +42,8 @@ int main()
 						world[y][x] = rand() < RAND_MAX / 10 ? 1 : 0;
 		
 		draw(world, w, h);
+		evolution(world, w, h);
+		draw(world, w, h);
 		return 0;
+
 }
